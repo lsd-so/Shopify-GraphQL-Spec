b6edf90dc91661b867f5b24c9cc4c3fc0db336eb
Yev Barkalov <yevbar1729@gmail.com>
2025-04-21 21:10:02 -0700

    gonna merge soon

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="shopagent",
-    version="0.12.0",
+    version="1.0.0",
     packages=['shopagent', 'shopagent.api', 'shopagent.code_gen', 'shopagent.constants', 'shopagent.lsd', 'shopagent.models', 'shopagent.nav']
 )
